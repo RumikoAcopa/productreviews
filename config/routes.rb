03f396618257bf257a2a820a01bdc198e854189e
rumikoacopa <rumikorios@gmail.com>
2021-01-06 13:02:38 -0500

    changed route path, renamed sign in/out, changed config.navigational_formats to empty

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',     
+    sign_out: 'logout',
+    registration: 'signup',
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  #path removes /users 4rm route path when you look at route/ info/routes in localhost:3000
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
