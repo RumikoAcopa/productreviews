3e39996b2ace537dc2178af0a7399c6b29d02561
rumikoacopa <rumikorios@gmail.com>
2021-01-07 19:27:33 -0500

    added to devise, serializer

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,12 @@
+class UserSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :id, :email, 
+  attribute :created_date do |user|
+    user && user.created_at.strftime('%d/%m/%Y')
+  end
+  
+
+  #UserSerializer.new(resource).serializable_hash[:data][:attributes]
+  #UserSerializer.new(resource).serializable_hash[:data].map{|data| data[:attributes]}
+  #The 1st one will access serializer data for single record, the 2nd is to access multiple records
+end 
