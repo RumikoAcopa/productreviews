b9e98885eadda0edead6a3989dc049241a818693
rumikoacopa <rumikorios@gmail.com>
2021-01-12 00:32:42 -0500

    user serializer unchanged

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,9 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :email, 
-  attribute :created_date do |user|
-    user && user.created_at.strftime('%d/%m/%Y')
-  end
+  attributes :id, :email
+  #attributes :created_date do |user|
+  #  user && user.created_at.strftime('%d/%m/%Y')
+  #end
   
 
   #UserSerializer.new(resource).serializable_hash[:data][:attributes]
