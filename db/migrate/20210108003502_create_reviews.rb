ff7efe496670e1afa2e91cd702a4b4788e51638a
rumikoacopa <rumikorios@gmail.com>
2021-01-14 20:03:02 -0500

    no change

--- db/migrate/20210108003502_create_reviews.rb
+++ db/migrate/20210108003502_create_reviews.rb
@@ -4,7 +4,6 @@ class CreateReviews < ActiveRecord::Migration[6.0]
       t.string :comment
       t.references :user, null: false, foreign_key: true
       t.references :product, null: false, foreign_key: true
-
       t.timestamps
     end
   end
